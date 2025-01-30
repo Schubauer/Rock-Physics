304cd026c49226dd6fa2d1ce52cc4b4c065fef00
Schubauer <thomas.schubauer@gmail.com>
2025-01-30 15:07:03 +0100

    Add files via upload

--- Ex1RP.py
+++ Ex1RP.py
@@ -10,8 +10,7 @@ rho2 = 1000  #Density fluid (kg/m^3)
 K_eff = K1 
 mu_eff = mu1
 n = 1
-i = 1
-por = 1
+
 #Creating the list
 Vp_list = []
 Vs_list = []
@@ -20,33 +19,27 @@ Vs_Slist = []
 phi_list = []
 
 for n in range(100):
-    phi = por*(n/100) #Changes the x scale of the plot
+    phi = 0.5*(n/100) #Changes the x scale of the plot
 
     #Direct calc.
     K_direct = K1 * (1 - phi) + K2 * phi
     mu_direct = mu1 * (1 - phi)
     rho_direct = rho1 * (1 - phi) + rho2 * phi
 
+    K_eff = K1 * (1 - phi) / (1 + phi * (K1 - K_eff) / (K1 + 4/3 * mu1))
+    mu_eff = mu1 * (1 - phi) / (1 + phi * (mu1 - mu_eff) / (mu1 + mu_eff))
+    rho_sca = rho1 * (1 - phi) + rho2 * phi
+
     Vp_direct = ((K_direct + 4/3 * mu_direct) / rho_direct)**(1/2)
     Vs_direct = (mu_direct / rho_direct)**(1/2)
     Vp_list.append(Vp_direct)
     Vs_list.append(Vs_direct)
 
-    phi_list.append(phi)
-
-for i in range(100):
-    phi2 = por*(i/100)
-    #SCA calc.
-    for n in range(100):
-        esh_ratio = (3*K_eff + 4*mu_eff) / (3*K2 + 4*mu_eff) #e2/e fraction
-        K_eff = K1 + (K2 - K1)*esh_ratio*phi2
-        mu_eff = mu1 + (0 - mu1)*esh_ratio*phi2
-    
-    rho_sca = rho1 * (1 - phi) + rho2 * phi
     Vp_sca = ((K_eff + 4/3 * mu_eff) / rho_sca)**(1/2)
     Vs_sca = (mu_eff / rho_sca)**(1/2)
     Vp_Slist.append(Vp_sca)
     Vs_Slist.append(Vs_sca)
+    phi_list.append(phi)
 
 # Plot results
 plt.plot(phi_list, Vp_list, label='Vp - Direct', linestyle='dashed')
